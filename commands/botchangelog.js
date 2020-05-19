99023bf2427f0727045da58f79d48ae964177201
AlexDiego123 <48864944+AlexDiego123@users.noreply.github.com>
2020-05-19 18:51:57 +0200

    updated botchangelog command

--- commands/botchangelog.js
+++ commands/botchangelog.js
@@ -5,7 +5,7 @@ module.exports = {
         const changelog = new Discord.MessageEmbed()
         .setColor('0000FF')
         .setTitle('**v2.5 BETA**')
-        .setDescription('Added The Command **createrole**\nAdded The Command **createtextchannel**\nAdded The Command **createvoicechannel**\nAdded The Command **mute**\nAdded The Command **unmute**\nAdded The Command **botchangelog**\n\n**New Update Of The Version v2.5 BETA**\n\n**New Update Of The Version v2.5 BETA**\nAdded The Command **ban-id**\nAdded The Command **unban**\nAdded The Command **deleterole**\n\n**New Update Of The Version v2.5 BETA**\nAdded The Command **kick-id**\n\n**New Update Of The Version v2.5 BETA**\nTotal Roles Added To The Command **server-info**\nAdded The Command **role-info**')
+        .setDescription('Added The Command **createrole**\nAdded The Command **createtextchannel**\nAdded The Command **createvoicechannel**\nAdded The Command **mute**\nAdded The Command **unmute**\nAdded The Command **botchangelog**\n\n**New Update Of The Version v2.5 BETA**\n\nAdded The Command **ban-id**\nAdded The Command **unban**\nAdded The Command **deleterole**\n\n**New Update Of The Version v2.5 BETA**\nAdded The Command **kick-id**\n\n**New Update Of The Version v2.5 BETA**\nTotal Roles Added To The Command **server-info**\nAdded The Command **role-info**\n\n**New Update Of The Version v2.5 BETA**\nAdded The Command **8ball**\nModified The **help** command\nAdded The Command **utility**\nAdded The Command **moderation**\nAdded The Command **games**\nAdded The Command **config**\nAdded The Command **botinfo**\nAdded The Command **invite**')
         .setTimestamp()
         .setFooter(`Command Executed By: ${message.author.username}`);
             
