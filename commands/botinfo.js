0a5a8d114cd9897168ff237b5110cd664fefd635
AlexDiego123 <48864944+AlexDiego123@users.noreply.github.com>
2020-05-19 18:51:18 +0200

    updated botinfo command

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -9,7 +9,8 @@ module.exports = {
 		.setDescription(`**Categories**`)
 		.addFields(
 			{ name: ':bar_chart: botstats', value: 'Displays The Bot Stats.'},
-			{ name: ':page_with_curl: botchangelog', value: 'Displays The Bot Changelog.'}
+			{ name: ':page_with_curl: botchangelog', value: 'Displays The Bot Changelog.'},
+			{ name: ':mailbox_with_mail: invite', value: 'Displays The Link To Invite The Bot In Other Servers'}
 		)
 		.setTimestamp()
 		.setFooter(`Command Executed By: ${message.author.username}`);
