73acdf773a685ee09f465a397506e66ee9649625
AlexDiego123 <48864944+AlexDiego123@users.noreply.github.com>
2020-05-19 14:03:56 +0200

    updated help command

--- commands/help.js
+++ commands/help.js
@@ -1,43 +1,25 @@
-const { prefix } = require('../config.json');
-
+const Discord = require('discord.js')
 module.exports = {
 	name: 'help',
 	aliases: ['commands'],
+	guildOnly: 'true',
 	usage: '[Command Name]',
 	execute(message, args) {
-		const data = [];
-		const { commands } = message.client;
+		const helpembed = new Discord.MessageEmbed()
+		.setColor('00ff00')
+		.setTitle(`Here Is A List Of All My Commands`)
+		.setDescription(`**Categories**`)
+		.addFields(
+			{ name: ':hammer: Moderation', value: 'Moderation Category'},
+			{ name: ':gear: Config', value: 'Config Category'},
+			{ name: ':video_game: Games', value: 'Games Category'},
+			{ name: ':information_source: Bot Info', value: 'Bot Info Category'},
+			{ name: ':wrench: Utility', value: 'Utility Category'}
+		)
+		.setTimestamp()
+		.setFooter(`Command Executed By: ${message.author.username}`);
 
-		if (!args.length) {
-			data.push('Here Is A List Of All My Commands');
-			data.push(commands.map(command => command.name).join(', '));
-			data.push(`\nYou Can Type \'${prefix}help [Command Name]\' To Get Info About A Specific Command`);
 
-			return message.author.send(data, { split: true })
-				.then(() => {
-					if (message.channel.type === 'dm') return;
-					message.reply('I Sent You A DM Message With A List Of All My Commands');
-				})
-				.catch(error => {
-					message.reply('I Can\'t Send You A DM Message');
-				});
-		}
-
-		const name = args[0].toLowerCase();
-		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-		if (!command) {
-			return message.reply('That Command Is Not Valid');
-		}
-
-		data.push(`**Name:** ${command.name}`);
-
-		if (command.aliases) data.push(`**Alias:** ${command.aliases.join(', ')}`);
-		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Correct Usage:** ${prefix}${command.name} ${command.usage}`);
-
-		data.push(`**Cooldown:** ${command.cooldown || 3} Second(s)`);
-
-		message.channel.send(data, { split: true });
+        message.channel.send(helpembed)
 	},
 };
